8d49390567606cdb50f5f049c607d40daa85435d
piaocreative <johnpiao171019@gmail.com>
2021-09-08 15:45:20 -0400

    Update issues

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $( document ).ready( function() {
         $( '.logos' ).slick({
             infinite: true,
             slidesToShow: 5,
-            slidesToScroll: 5,
+            slidesToScroll: 1,
             dots: false,
             arrows: false,
             responsive: [
@@ -13,7 +13,7 @@ $( document ).ready( function() {
                     settings: {
                         infinite: true,
                         slidesToShow: 4,
-                        slidesToScroll: 4,
+                        slidesToScroll: 1,
                     }
                 },
                 {
@@ -21,7 +21,7 @@ $( document ).ready( function() {
                     settings: {
                         infinite: true,
                         slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToScroll: 1,
                     }
                 },
                 {
@@ -29,7 +29,65 @@ $( document ).ready( function() {
                     settings: {
                         infinite: true,
                         slidesToShow: 2,
-                        slidesToScroll: 2,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+    } 
+
+    if ( $( '#carouselBestSelling' ).length ) {
+        $( '#carouselBestSelling' ).slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            autoplay: true,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+    }
+
+    if ( $( '#posts' ).length ) {
+        $( '#posts' ).slick({
+            infinite: true,
+            slidesToShow: 3.1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            responsive: [
+                {
+                    breakpoint: 1400,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 1300,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
                     }
                 }
             ]
